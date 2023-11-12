0a477e3abfe59ed85348cbf196dae43451a04bbb
lukabuts <butskhrikidzeluka0@gmail.com>
2023-11-12 20:18:52 +0400

    Changed Styles

--- src/Components/Thanks.jsx
+++ src/Components/Thanks.jsx
@@ -4,7 +4,9 @@ import thanksImg from '../imgs/thanks.svg'
 const Thanks = ({rate, btns, setSubmited, submited}) => {
     return(
         <div className='thanks-div'>
-            <img src={thanksImg} alt="Mobile" /> 
+            <div className="img-div">
+                <img src={thanksImg} alt="Mobile" /> 
+            </div>
             <p className="how-much-p">You Selected {rate} out of {btns[btns.length - 1]}</p>
             <h1 className="thanks-h1">
                 Thank you!
